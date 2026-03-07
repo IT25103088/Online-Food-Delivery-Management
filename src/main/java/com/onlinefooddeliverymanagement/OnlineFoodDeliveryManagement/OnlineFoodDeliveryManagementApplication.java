6a77039397d616de5d4b1b720e68589b123725dc
IT25103088 <it25103088@my.sliit.lk>
2026-03-07 15:36:35 +0530

    Initial project structure

---
+++ src/main/java/com/onlinefooddeliverymanagement/OnlineFoodDeliveryManagement/OnlineFoodDeliveryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.onlinefooddeliverymanagement.OnlineFoodDeliveryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodDeliveryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodDeliveryManagementApplication.class, args);
+	}
+
+}
