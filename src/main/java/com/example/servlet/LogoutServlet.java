a4ef5a73cbdef81999fa1dba4e9204f6d38a0e74
IT25103135 - Nizla Ameer <it25103135@my.sliit.lk>
2026-05-23 23:47:32 +0530

    Update LogoutServlet.java

--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpSession;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        HttpSession session = req.getSession(false);
+        HttpSession session = req.getSession(false)
         if (session != null) session.invalidate();
         res.sendRedirect("component4.html");
     }
