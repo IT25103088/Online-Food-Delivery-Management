97b58cd0b22efafee97afa11da275f1e6317b8c7
IT25103135 - Nizla Ameer <it25103135@my.sliit.lk>
2026-05-23 23:47:19 +0530

    Add IOException import to LogoutServlet

--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -3,6 +3,7 @@ package com.example.servlet;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
