542fb9951b3f1cdfd49c2e11a2e6095f361d06cd
IT25103135 - Nizla Ameer <it25103135@my.sliit.lk>
2026-05-23 23:47:04 +0530

    Remove unused import in LogoutServlet

--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.io.IOException;
+
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
